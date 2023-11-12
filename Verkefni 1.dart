58ea1a8fb6ceca2ad1ff8a3f4497c1ef0ed50ffe
sunnadjonsdottir <146673401+sunnadjonsdottir@users.noreply.github.com>
2023-11-12 23:55:37 +0000

    Create Verkefni 1.dart

---
+++ Verkefni 1.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  String yourName = "Sunna";
+  print("Hello");
+  print(yourName);
+
+  int num1 = 5;
+  int num2 = 10;
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: $sum");
+
+  double dividend = 20.0;
+  double divisor = 4.0;
+  double result = dividend / divisor;
+  print("The result of $dividend divided by $divisor is: $result");
+
+  int result1 = -1 + 5 * 5;
+  int result2 = (25 + 5) % 4;
+  double result3 = 8 + 6 * 4 / 2;
+  double result4 = 2 + 15 / 6 * 1 - 7 % 2;
+
+  print("Result 1: -1 + 5 * 5 = $result1");
+  print("Result 2: (25 + 5) % 4 = $result2");
+  print("Result 3: 8 + 6 * 4 / 2 = $result3");
+  print("Result 4: 2 + 15 / 6 * 1 - 7 % 2 = $result4");
+}
